46cebabbc4a928f573573b0623b45aba01656d33
ubuntu-desktop <mxb1999@gmail.com>
2021-02-02 08:09:49 -0500

    OS issues again

--- src/fit/CoefficientFit.java
+++ src/fit/CoefficientFit.java
@@ -4,14 +4,14 @@ import java.util.concurrent.ConcurrentHashMap;
 public class CoefficientFit
 {
     private fitFunc func;
-    private ConcurrentHashMap<String, double[]> map;
+    private ConcurrentHashMap<String, double[]> map;//dereference variable values based upon name, essential for custom functionality. Assuming (relatively) low numbers of variables
     private String ind;//store the name of the independent variable(s) in fit
     private String dep;//store the name of the dependent variable(s) in fit
-    public void runFunc()
+    public void runFunc()//run the fitFunc function
     {
         func.run(map, ind, dep);
     }
-    public CoefficientFit(fitFunc f, ConcurrentHashMap<String, double[]> m, String i, String d)
+    public CoefficientFit(fitFunc f, ConcurrentHashMap<String, double[]> m, String i, String d)//Constructor
     {
         this.func = f;
         this.map = m;
--- src/fit/fitFunc.java
+++ src/fit/fitFunc.java
@@ -1,5 +1,6 @@
 package fit;
 import java.util.concurrent.ConcurrentHashMap;
+//interface to allow for runnable objects with arguments, used for passing data for fits
 public interface fitFunc extends Runnable 
 {
     public void run(ConcurrentHashMap<String, double[]> q, String independent, String dependent);
